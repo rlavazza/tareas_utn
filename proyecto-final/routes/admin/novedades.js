ae20cf6e68cea16ee63995835a690ec9a765d491
Roxana Lavazza <rlavazza@gmail.com>
2023-05-30 01:23:38 -0300

    login-out

--- proyecto-final/routes/admin/novedades.js
+++ proyecto-final/routes/admin/novedades.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('admin/novedades',{
     layout:'admin/layout',
-    cliente: req.session.nombre
+    empleado: req.session.nombre
   });
 });
 
